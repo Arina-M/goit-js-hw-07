2dfa4025bf1cf2ec836c7f0fdb51a27a9aa4b870
Arina Miahkykh <79450238+Arina-M@users.noreply.github.com>
2023-04-24 16:37:21 +0300

    upd task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
-
+//console.log(galleryItems);
 
 const galleryList = document.querySelector('.gallery');
 galleryList.insertAdjacentHTML('beforeend', createGalleryItem(galleryItems));
@@ -23,24 +21,27 @@ galleryList.addEventListener('click', onClick);
 
 function onClick(event) {
   event.preventDefault();
+  //не працює, якщо такий запис в if(event.target.nodeName !== 'gallery__image')    
+  //не розумію як переробити  :(
   if (!event.target.classList.contains('gallery__image')) {
     return;
   }
-  const modalWindowImg = basicLightbox.create(`
-      <img src="${event.target.dataset.source}" width="800" height="600">
-  `);
-  modalWindowImg.show(galleryList.addEventListener('click', closeImgByClick));
-  
-  function closeImgByClick() {
-    modalWindowImg.close(galleryList.removeEventListener('keydown', closeImgByBtn));
-  }
 
-  // clicking Escape
-  galleryList.addEventListener('keydown', closeImgByBtn);
-  
-  function closeImgByBtn(event) {
-    if (event.code === 'Escape') {
-      modalWindowImg.close(galleryList.removeEventListener('keydown', closeImgByBtn));
-    }
+  const sourceImage = event.target.dataset.source ?? target.closest('img').dataset.source;
+  const currentOriginal = galleryItems.find(({original}) => original === sourceImage);
+
+  const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" width="800" height="600">`,{
+        onShow: () => window.addEventListener('keydown', onEscClick),
+        onClose: () => window.removeEventListener('keydown', onEscClick),
+      }
+  );
+
+  instance.show();
+
+  function onEscClick(event) {
+      if(event.code === 'Escape'){
+           instance.close();
+       }
   }
 }
